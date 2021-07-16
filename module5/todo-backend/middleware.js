7e9521f6a8f8a0fa774fbb2dbd49128f596ab9e3
Ashley Stanley <astanley006@gmail.com>
2021-07-16 13:10:50 -0400

    todos-backend done, added comment

--- module5/todo-backend/middleware.js
+++ module5/todo-backend/middleware.js
@@ -1,1 +1,17 @@
+// Can't figure out how to link this to my server as a separate file. Will look again later
 
+// const express = require("express")
+// const app = express()
+// const todosRouter = require("./router")
+
+// module.exports = () => {
+//     app.use(express.json())
+    
+//     app.use("/", todosRouter)
+
+//     app.use((req, res, next)=>{
+//         console.log("middleware executed!")
+//         next()
+//     })
+    
+// }
