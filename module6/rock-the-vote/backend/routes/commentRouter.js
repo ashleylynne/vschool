69601e1e846f1a004cb89a6b40cc9d6abc5e32ea
Ashley Stanley <astanley006@gmail.com>
2021-11-17 14:27:39 -0500

    trying to figure out get requests rtv backend

--- module6/rock-the-vote/backend/routes/commentRouter.js
+++ module6/rock-the-vote/backend/routes/commentRouter.js
@@ -1,12 +1,11 @@
 const express = require("express")
 const commentRouter = express.Router()
 const Comment = require("../models/Comment.js")
-const jwt = require("jsonwebtoken")
+// const jwt = require("jsonwebtoken")
 
-// get all comments (not sure if I need this but will build just in case)
+// get all comments based on issue
 commentRouter.get("/:issueId", (req, res, next) => {
-    Comment.find(
-        {_id: req.params.issueId}, //I'm thinking we do comments by each issue bc comments by user doesn't make much sense
+    Comment.find({_id: req.params.issueId}, //I'm thinking we do comments by each issue bc comments by user doesn't make much sense
         (err, comments) => {
         if(err){
             res.status(500)
@@ -15,7 +14,7 @@ commentRouter.get("/:issueId", (req, res, next) => {
         return res.status(200).send(comments)
     })
 })
-
+// post one comment
 commentRouter.post("/:issueId", (req, res, next) => {
         req.body.user = req.user._id
         req.body.issueId = req.params.issueId
@@ -33,8 +32,7 @@ commentRouter.post("/:issueId", (req, res, next) => {
 commentRouter.delete("/:issueId/:commentId", (req, res, next) => {
     Comment.findOneAndDelete({
         _id: req.params.commentId,
-        user: req.user._id,
-        issueId: req.params.issueId
+        user: req.user._id
     },
         (err, deletedComment) => {
             if(err){
@@ -46,9 +44,11 @@ commentRouter.delete("/:issueId/:commentId", (req, res, next) => {
 })
 
 // update aka the devil
-commentRouter.put("/:commentId", (req, res, next) => {
-    Comment.findOneAndUpdate(
-        {id: req.params.commentId, user: req.user._id},
+commentRouter.put("/:issueId/:commentId", (req, res, next) => {
+    Comment.findOneAndUpdate({
+        _id: req.params.commentId, 
+        user: req.user._id
+    },
         req.body,
         {new: true},
         (err, updatedComment) => {
