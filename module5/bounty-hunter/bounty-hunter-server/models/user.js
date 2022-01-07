ef3cc43e0d1939c784ae1dbd22ca164da48f8944
Ashley Stanley <astanley006@gmail.com>
2022-01-07 12:42:45 -0500

    bounty user auth

---
+++ module5/bounty-hunter/bounty-hunter-server/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+// user model
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    memberSince: {
+        type: Date,
+        default: Date.now,
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    }
+})
+
+
+
+module.exports = mongoose.model("User", userSchema)
+
